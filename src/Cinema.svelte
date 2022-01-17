c6eaac88e3b9496ab6625b66d7dd4812fc8b1446
Aida Manna <aida.mannaloyarte@thoughtworks.com>
2022-01-17 21:18:32 +0100

    Implement button to fetch the cinema and render heading

---
+++ src/Cinema.svelte
@@ -0,0 +1,24 @@
+<script>
+  import * as cinemaClient from './clients/cinemaClient.js';
+
+  let disabled = false;
+  let fetchCinema;
+
+  const handleGetRecosClick = async () => {
+    disabled = true;
+
+    fetchCinema = cinemaClient.getCinemaJson();
+  }
+
+</script>
+
+<button on:click={ handleGetRecosClick } { disabled }>Get recos</button>
+{#if disabled}
+  {#await fetchCinema then cinema}
+    <h1>{ cinema.headline }</h1>
+  {/await}
+{/if}
+
+<style>
+
+</style>
