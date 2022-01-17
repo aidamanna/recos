d659f87cf8315d9beee11f0b04202f48383d40fc
Aida Manna <aida.mannaloyarte@thoughtworks.com>
2022-01-17 16:23:49 +0100

    Initialize hello world svelte project

---
+++ test/App.spec.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/svelte'
+import App from '../src/App.svelte'
+
+test('shows proper heading when rendered', () => {
+  const { getByText } = render(App, { name: 'World' });
+
+  expect(getByText('Hello World!')).toBeInTheDocument();
+})
