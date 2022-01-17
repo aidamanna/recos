1e92c00bd666bd15cbcb2c331faf613e15ef9146
Aida Manna <aida.mannaloyarte@thoughtworks.com>
2022-01-17 21:18:32 +0100

    Create button and disable when clicking

---
+++ src/Cinema.svelte
@@ -0,0 +1,16 @@
+<script>
+  import * as cinemaClient from './clients/cinemaClient.js';
+
+  let disabled = false;
+
+  const handleGetRecosClick = async () => {
+    disabled = true;
+  }
+
+</script>
+
+<button on:click={ handleGetRecosClick } { disabled }>Get recos</button>
+
+<style>
+
+</style>
